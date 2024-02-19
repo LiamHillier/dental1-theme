33c8530726cd4f0c8eafe5ea95a56ff257919549
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-02-19 15:10:25 +1100

    autoheight

--- theme/header.php
+++ theme/header.php
@@ -31,6 +31,7 @@
 
     <?php wp_head(); ?>
     <script src="https://unpkg.com/embla-carousel/embla-carousel.umd.js"></script>
+    <script src="https://unpkg.com/embla-carousel-auto-height/embla-carousel-auto-height.umd.js"></script>
 
 
 
