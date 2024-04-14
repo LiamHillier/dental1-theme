8ed8fcb225d2e21103715bfc80c5f76908e720f9
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-04-14 16:05:29 +1000

    Update header-content.php

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -17,7 +17,7 @@ $logo = file_get_contents('wp-content/themes/dental1/theme/assets/logos/dental1-
 		<div class="logo h-full flex justify-start items-center">
 			<a href="/"><?php echo $logo; ?></a>
 		</div>
-		<nav id="site-navigation" aria-label="<?php esc_attr_e('Main Right  Navigation', 'dental1'); ?>" class="hidden md:block">
+		<nav id="site-navigation" aria-label="<?php esc_attr_e('Main Right  Navigation', 'dental1'); ?>" class="hidden lg:block">
 			<?php
 			wp_nav_menu(
 				array(
@@ -29,7 +29,7 @@ $logo = file_get_contents('wp-content/themes/dental1/theme/assets/logos/dental1-
 			);
 			?>
 		</nav>
-		<div class="md:hidden flex items-center gap-4">
+		<div class="lg:hidden flex items-center gap-4">
 			<a class=" button primary" href="/book-appointment">book now</a>
 			<div class="flex justify-end"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-menu">
 					<line x1="3" y1="12" x2="21" y2="12" />
@@ -44,7 +44,7 @@ $logo = file_get_contents('wp-content/themes/dental1/theme/assets/logos/dental1-
 
 </header><!-- #masthead -->
 
-<div class="mobile-menu hidden md:hidden flex flex-col items-stretch justify-start fixed w-screen h-screen bg-white top-0 z-50 pt-24 px-10">
+<div class="mobile-menu  lg:hidden flex flex-col items-stretch justify-start fixed w-screen h-screen bg-white top-0 z-50 pt-24 px-10">
 	<div class="w-12 absolute top-14 left-10">
 		<?php echo file_get_contents('wp-content/themes/dental1/theme/assets/logos/icon.svg'); ?>
 	</div>
