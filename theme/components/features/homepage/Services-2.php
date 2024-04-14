7b5fa9cd4f71930aa8514d8edeaea8d71a3c49f1
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-04-14 11:22:55 +1000

    Update Services-2.php

--- theme/components/features/homepage/Services-2.php
+++ theme/components/features/homepage/Services-2.php
@@ -1,4 +1,5 @@
-<section id="services" class="text-center relative bg-white -mt-1 py-20 md:pb-40 md:pt-40  grid md:grid-cols-2 max-w-screen-2xl mx-auto px-5  md:px-20 gap-10">
+<section id="services" class="text-center relative bg-white -mt-1 py-20 md:pb-40 md:pt-40 ">
+    <div class=" grid md:grid-cols-2 max-w-screen-2xl mx-auto px-5  md:px-20 gap-10">
     <div class=" text-left  mb-16 md:mb-0 flex flex-col ">
         <div>
             <p class="mb-2 text-primary animate__initial animate__animated animate__fadeInLeft">Our services
@@ -27,6 +28,8 @@
         </div>
 
     </div>
+    </div>
+
     <div class="service-shape-divider-bottom-1 absolute bottom-6 left-0">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
