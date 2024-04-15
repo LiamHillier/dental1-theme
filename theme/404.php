cf3567e76bdd99ac8fe6cf1b8175743d1878fb29
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-04-15 14:05:41 +1000

    Update 404.php

--- theme/404.php
+++ theme/404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -10,22 +11,22 @@
 get_header();
 ?>
 
-	<section id="primary">
-		<main id="main">
-
+<section id="primary">
+	<main id="main">
+		<div class="max-w-screen-2xl mx-auto px-5 md:px-20 h-screen flex items-center justify-center">
 			<div>
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Page Not Found', 'dental1' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e('Page Not Found', 'dental1'); ?></h1>
 				</header><!-- .page-header -->
 
-				<div <?php dental1_content_class( 'page-content' ); ?>>
-					<p><?php esc_html_e( 'This page could not be found. It might have been removed or renamed, or it may never have existed.', 'dental1' ); ?></p>
+				<div <?php dental1_content_class('page-content'); ?>>
+					<p><?php esc_html_e('This page could not be found. It might have been removed or renamed, or it may never have existed.', 'dental1'); ?></p>
 					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</div>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+		</div>
+	</main><!-- #main -->
+</section><!-- #primary -->
 
 <?php
 get_footer();
