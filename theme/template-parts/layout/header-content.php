2463ac9b97f33a36accbeb2a173134e81ce59d3f
LiamHillier <hillierliam37@gmail.com>
2024-06-06 09:58:32 +1000

    Update header-content.php

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -92,7 +92,7 @@ class Walker_Nav_Menu_Details extends Walker_Nav_Menu
 	<div class=" max-w-screen-2xl  mx-auto px-5 md:px-20 py-2 text-sm flex justify-between gap-4">
 		<div class="flex gap-4 items-center">
 			<div class="flex gap-1 items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mt-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
 					<path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
 					<path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
 				</svg>
