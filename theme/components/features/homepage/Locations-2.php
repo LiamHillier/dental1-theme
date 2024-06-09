eb3ef416524c31d9448eb0b896348e6c540b3e1a
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-06-09 19:02:02 +1000

    Update Locations-2.php

--- theme/components/features/homepage/Locations-2.php
+++ theme/components/features/homepage/Locations-2.php
@@ -36,7 +36,7 @@
                         </svg>
                     </div>
                 </div>
-                <div id="loading-spinner" class="hidden absolute top-4 right-4">
+                <div id="loading-spinner" class="hidden mt-4">
                     <svg class="animate-spin h-5 w-5 text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291a7.964 7.964 0 01-2-5.291H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
