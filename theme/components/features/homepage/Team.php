43f0262134170f5cfd01499fdfe80c41e97e9667
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-06-09 19:20:37 +1000

    Update Team.php

--- theme/components/features/homepage/Team.php
+++ theme/components/features/homepage/Team.php
@@ -10,101 +10,94 @@
                         </svg></span></span>
             </h2>
         </header>
-     
 
-       
+
+
     </div>
 
     <div class="team mt-12 max-w-screen-2xl mx-auto relative">
-    <button class="team__prev absolute left-2 top-[50%] z-10  bg-primary text-white rounded-full p-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                    <path fill-rule="evenodd" d="M20.25 12a.75.75 0 01-.75.75H6.31l5.47 5.47a.75.75 0 11-1.06 1.06l-6.75-6.75a.75.75 0 010-1.06l6.75-6.75a.75.75 0 111.06 1.06l-5.47 5.47H19.5a.75.75 0 01.75.75z" clip-rule="evenodd" />
-                </svg>
-            </button>
-            <button class="team__next absolute right-2 top-[50%] z-10 bg-primary text-white rounded-full p-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                    <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
-                </svg>
-            </button>
+        <button class="team__prev absolute left-2 top-[50%] z-10  bg-primary text-white rounded-full p-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M20.25 12a.75.75 0 01-.75.75H6.31l5.47 5.47a.75.75 0 11-1.06 1.06l-6.75-6.75a.75.75 0 010-1.06l6.75-6.75a.75.75 0 111.06 1.06l-5.47 5.47H19.5a.75.75 0 01.75.75z" clip-rule="evenodd" />
+            </svg>
+        </button>
+        <button class="team__next absolute right-2 top-[50%] z-10 bg-primary text-white rounded-full p-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+            </svg>
+        </button>
         <div class="team__viewport">
             <div class="team__container">
                 <div class="team__slide ">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/12.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Michael Ahmar</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="team__slide  animate__delay-0_2s">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/13.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Ahmad Mehrez</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="team__slide  animate__delay-0_4s">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/14.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Daniel Yacoub</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="team__slide  animate__delay-0_8s">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/15.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Daniel Karam</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="team__slide  animate__delay-1s">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/16.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Adrian Balendra</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="team__slide  animate__delay-1_2s">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/17.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Filomena Perri</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <div class="team__slide  animate__delay-1_4s">
-                    <div class="slide-content">
+                    <a href="/meet-the-team" class="slide-content">
                         <img src="/wp-content/uploads/2024/05/4.png" />
                         <div>
                             <p class="font-semibold text-lg">Dr Francis Tenazas</p>
                             <p>General Dentist</p>
-                            <a href="/meet-the-team/" class="button primary">Learn More</a>
+
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="flex justify-center mt-16">
-    <a href="/our-team/" class="link mt-6 animate__initial animate__animated animate__fadeInUp animate__delay-1s">View all
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block ml-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                </svg>
-            </a>
     </div>
     <div class="mx-5 md:mx-20 border-b border-neutral-300 mt-10" />
 </section>
