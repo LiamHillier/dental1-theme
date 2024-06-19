dbf66bd852b3a0b7c9d5c5e1c6168e7631169eb5
liamhillier <hillierliam37@gmail.com>
2024-06-19 16:49:59 +1000

    page banners

--- theme/template-parts/content/content-page.php
+++ theme/template-parts/content/content-page.php
@@ -8,16 +8,20 @@
  * @package dental1
  */
 
-	?>
+?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div id="banner" class="relative overflow-hidden min-h-[276px] md:min-h-[400px]">
+	<div id="banner" class="relative overflow-hidden min-h-[276px] md:min-h-[400px] flex items-center justify-center">
 		<div class="bg-gradient-to-t from-transparent to-white h-[180px] w-full absolute top-0 left-0 z-10"></div>
+		<div class="bg-gradient-to-t from-white to-transparent h-full w-full absolute bottom-0 left-0 z-10"></div>
 		<div class="absolute top-0 left-0 w-full h-full object-cover">
 			<?php dental1_post_thumbnail(); ?>
 		</div>
-		
+
+		<div class="relative z-10">
+			<h1 class="text-center font-bold pt-8 pb-10 text-primary"><?php echo get_the_title(); ?></h1>
+		</div>
 		<div class="service-shape-divider-bottom-1 absolute bottom-6 left-0 ">
 			<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 				<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="fill-gray-100"></path>
@@ -30,10 +34,10 @@
 			</svg>
 		</div>
 	</div>
-	
+
 	<div class="relative z-10 flex flex-col items-center justify-center gap-4">
-			<h1 class="text-center font-bold pt-8 pb-10 text-primary"><?php echo get_the_title(); ?></h1>
-		</div>
+
+	</div>
 
 
 	<div class="max-w-screen-2xl mx-auto px-5 md:px-20" <?php dental1_content_class('entry-content'); ?>>
