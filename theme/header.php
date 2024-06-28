7bee998c74c5eee0a9654b11a4210a3fbb973161
liamhillier <hillierliam37@gmail.com>
2024-06-28 13:10:28 +1000

    locations shortcode

--- theme/header.php
+++ theme/header.php
@@ -19,13 +19,31 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Hotjar Tracking Code for https://dental1.com.au/ -->
+    <script>
+        (function(h, o, t, j, a, r) {
+            h.hj = h.hj || function() {
+                (h.hj.q = h.hj.q || []).push(arguments)
+            };
+            h._hjSettings = {
+                hjid: 3612248,
+                hjsv: 6
+            };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    </script>
+
 
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCw4mjDWv-Bat8U9jtuw2mx0C4hdnKvCUA&libraries=places" async defer></script>
-    
+
     <script src="https://unpkg.com/embla-carousel/embla-carousel.umd.js"></script>
 
     <?php wp_head(); ?>
-    
+
 
 
 
