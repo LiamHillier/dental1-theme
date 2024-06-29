14f4af9bca30bc0f4d15336a5578443f6d7334f1
LiamHillier <hillierliam37@gmail.com>
2024-06-29 22:05:01 +1000

    Update header-content.php

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -104,7 +104,8 @@ $end_time = $enquiry_hours['end_time']; // Assuming format is 'H:i:s'
 $current_time = current_time('H:i:s');
 
 // Function to check if the current time is within the enquiry hours
-function is_within_enquiry_hours($current_time, $start_time, $end_time) {
+function is_within_enquiry_hours($current_time, $start_time, $end_time)
+{
     $current_time = strtotime($current_time);
     $start_time = strtotime($start_time);
     $end_time = strtotime($end_time);
@@ -120,26 +121,29 @@ function is_within_enquiry_hours($current_time, $start_time, $end_time) {
 // Determine the message to display
 if (is_within_enquiry_hours($current_time, $start_time, $end_time)) {
     $message = "Our team is available to help you now";
-} else {
-    $message = "Reach out now and our team will respond promptly";
+?>
+
+    <div id="top-bar" class="lg:hidden font-semibold w-full text-white bg-primary z-50 sticky top-0">
+        <div class="max-w-screen-2xl mx-auto px-5 md:px-20 py-2 text-sm flex justify-center gap-4">
+            <a href="/contact/" class="text-white text-sm font-semibold relative flex underline items-center gap-1">
+                <span class="absolute flex h-2 w-2 top-0 -right-2">
+                    <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                    <span class="relative inline-flex rounded-full h-2 w-2 bg-green-300"></span>
+                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16px" height="16px" viewBox="-32 0 512 512" class="w-4 h-4">
+                    <path d="M443.98 96.25c-11.01-45.22-47.11-82.06-92.01-93.72-32.19-8.36-63 5.1-89.14 24.33-3.25 2.39-6.96 3.73-10.5 5.48l28.32 18.21c7.42 4.77 9.58 14.67 4.8 22.11-4.46 6.95-14.27 9.86-22.11 4.8L162.83 12.84c-20.7-10.85-43.38-16.4-66.81-10.31-44.9 11.67-81 48.5-92.01 93.72-10.13 41.62-.42 80.81 21.5 110.43 23.36 31.57 32.68 68.66 36.29 107.35 4.4 47.16 10.33 94.16 20.94 140.32l7.8 33.95c3.19 13.87 15.49 23.7 29.67 23.7 13.97 0 26.15-9.55 29.54-23.16l34.47-138.42c4.56-18.32 20.96-31.16 39.76-31.16s35.2 12.85 39.76 31.16l34.47 138.42c3.39 13.61 15.57 23.16 29.54 23.16 14.18 0 26.48-9.83 29.67-23.7l7.8-33.95c10.61-46.15 16.53-93.16 20.94-140.32 3.61-38.7 12.93-75.78 36.29-107.35 21.95-29.61 31.66-68.8 21.53-110.43z" />
+                </svg>
+                <span class=""><?php echo $message; ?></span>
+            </a>
+        </div>
+    </div>
+
+<?php
 }
 ?>
 
-<div id="top-bar" class="lg:hidden font-semibold w-full text-white bg-primary z-50 sticky top-0">
-    <div class="max-w-screen-2xl mx-auto px-5 md:px-20 py-2 text-sm flex justify-center gap-4">
-        <a href="/contact/" class="text-white text-sm font-semibold relative flex underline items-center gap-1">
-            <span class="absolute flex h-2 w-2 top-0 -right-2">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-2 w-2 bg-green-300"></span>
-            </span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16px" height="16px" viewBox="-32 0 512 512" class="w-4 h-4">
-                <path d="M443.98 96.25c-11.01-45.22-47.11-82.06-92.01-93.72-32.19-8.36-63 5.1-89.14 24.33-3.25 2.39-6.96 3.73-10.5 5.48l28.32 18.21c7.42 4.77 9.58 14.67 4.8 22.11-4.46 6.95-14.27 9.86-22.11 4.8L162.83 12.84c-20.7-10.85-43.38-16.4-66.81-10.31-44.9 11.67-81 48.5-92.01 93.72-10.13 41.62-.42 80.81 21.5 110.43 23.36 31.57 32.68 68.66 36.29 107.35 4.4 47.16 10.33 94.16 20.94 140.32l7.8 33.95c3.19 13.87 15.49 23.7 29.67 23.7 13.97 0 26.15-9.55 29.54-23.16l34.47-138.42c4.56-18.32 20.96-31.16 39.76-31.16s35.2 12.85 39.76 31.16l34.47 138.42c3.39 13.61 15.57 23.16 29.54 23.16 14.18 0 26.48-9.83 29.67-23.7l7.8-33.95c10.61-46.15 16.53-93.16 20.94-140.32 3.61-38.7 12.93-75.78 36.29-107.35 21.95-29.61 31.66-68.8 21.53-110.43z" />
-            </svg>
-            <span class=""><?php echo $message; ?></span>
-        </a>
-    </div>
-</div>
-<header id="masthead" class="w-screen sticky top-[36px] lg:top-0 left-0 z-50 bg-transparent transition-colors duration-100 ease-in -mb-[112px]">
+
+<header id="masthead" class="w-screen sticky <?php is_within_enquiry_hours($current_time, $start_time, $end_time) ? 'top-[36px]' : '' ?> lg:top-0 left-0 z-50 bg-transparent transition-colors duration-100 ease-in -mb-[112px]">
     <div class="max-w-screen-2xl mx-auto py-4 px-5 md:px-20 flex justify-between items-center ">
         <div class="logo h-full flex justify-start items-center">
             <a href="/"><?php echo $logo; ?></a>
