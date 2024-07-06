0bd3771584167e403bb451bc846d7f70528df225
liamhillier <hillierliam37@gmail.com>
2024-07-06 17:30:14 +1000

    updates

--- theme/functions.php
+++ theme/functions.php
@@ -329,12 +329,12 @@ function locations_section()
 
             <!-- Locations carousel -->
             <section id="locations-carousel" class="embla relative locations-carousel  mt-6">
-            <button class="embla__prev absolute -left-4 md:-left-10 top-[50%] z-10 bg-primary text-white rounded-full p-2">
+            <button class="embla__prev absolute -left-8 md:-left-10 top-[50%] z-10 bg-primary text-white rounded-full p-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M20.25 12a.75.75 0 01-.75.75H6.31l5.47 5.47a.75.75 0 11-1.06 1.06l-6.75-6.75a.75.75 0 010-1.06l6.75-6.75a.75.75 0 111.06 1.06l-5.47 5.47H19.5a.75.75 0 01.75.75z" clip-rule="evenodd" />
                 </svg>
             </button>
-            <button class="embla__next absolute -right-4 md:-right-10 top-[50%] z-10 bg-primary text-white rounded-full p-2">
+            <button class="embla__next absolute -right-8 md:-right-10 top-[50%] z-10 bg-primary text-white rounded-full p-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
                 </svg>
