3fcf0ae5f12cb8578103f6c7311d1e6113726964
Liam Hillier <55169081+LiamHillier@users.noreply.github.com>
2024-07-30 15:13:19 +1000

    Update header.php

--- theme/header.php
+++ theme/header.php
@@ -47,6 +47,42 @@
     <!-- Embla Carousel -->
     <script async src="https://unpkg.com/embla-carousel/embla-carousel.umd.js"></script>
 
+    <!-- Facebook Pixel Code -->
+    <script async defer>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window,
+            document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '710783542309973', {}, {
+            "agent": "wordpress-6.4.5-1.6.0"
+        });
+
+        fbq('track', 'PageView', {
+            "source": "wordpress",
+            "version": "6.4.5",
+            "pluginVersion": "1.6.0"
+        });
+    </script>
+    <!-- DO NOT MODIFY -->
+    <!-- End Facebook Pixel Code -->
+    <!-- WordPress Facebook Integration end -->
+
+    <!-- Facebook Pixel Code -->
+
     <?php wp_head(); ?>
 
 
